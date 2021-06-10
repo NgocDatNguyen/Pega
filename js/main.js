5aa0cb9b013a2010463defea01b791c7a3c1e2c5
NgocDatNguyen <77161187+NgocDatNguyen@users.noreply.github.com>
2021-06-10 22:57:40 +0700

    first commit

---
+++ js/main.js
@@ -0,0 +1,22 @@
+
+$("#extend").click(function () {
+  $("#menu").toggle("show");
+});
+
+var swiper = new Swiper(".mySwiper", {
+  spaceBetween: 10,
+  slidesPerView: 4,
+  freeMode: true,
+  watchSlidesVisibility: true,
+  watchSlidesProgress: true,
+});
+var swiper2 = new Swiper(".mySwiper2", {
+  spaceBetween: 4,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  thumbs: {
+    swiper: swiper,
+  },
+});
